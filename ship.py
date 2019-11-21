cdb648cf922d96e9aea896f382f7fa08ef81a7af
nazgual <nazgual@ukr.net>
2019-11-21 19:56:21 +0200

    Added Starship moving

--- ship.py
+++ ship.py
@@ -3,8 +3,9 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         self.screen = screen  # Инициализирует корабль и задает его начальную позицию.
+        self.ai_settings = ai_settings
 
         # Загрузка изображения корабля и получение прямоугольника.
         self.image = pygame.image.load('images/starship.png')
@@ -13,6 +14,19 @@ class Ship():
         # Корабль появляется у нижнего края экрана.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        # Сохранение вещественной координаты центра корабля.
+        self.center = float(self.rect.centerx)
+
+        self.moving_right = False  # Флаг перемещения вправо
+        self.moving_left = False  # Флаг перемещения влево
+
+    def update(self):
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+        # Обновление атрибута rect на основании self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)  # Рисует корабль в начальной позиции.
